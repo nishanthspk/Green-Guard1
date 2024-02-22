d6339ef120f301a76e83cb4d8cc65d2121bce0f7
Naveen-gn <naveen2003gn@gmail.com>
2024-02-22 12:46:28 +0530

    Routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from './Components/Nav'
+import Awareness from './Pages/Awareness'
+import Complaints from './Pages/Complaints'
+import Guidelines from './Pages/Guidelines'
+import Home from './Pages/Home'
+import News from './Pages/News'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/news' element={<News />} />
+        <Route path='/awareness' element={<Awareness />} />
+        <Route path='/guidelines' element={<Guidelines />} />
+        <Route path='/complaints' element={<Complaints />} />
+      </Routes>
+      
+    </Router>
   )
 }
