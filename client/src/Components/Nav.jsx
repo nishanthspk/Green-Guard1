9f42ec4eb995a2843532576cf83efe608fc2e536
Naveen-gn <naveen2003gn@gmail.com>
2024-02-22 13:28:43 +0530

    Navbar adjacement

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -2,6 +2,9 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import Home from '../Pages/Home';
+import News from '../Pages/News';
 
 
 function myNavbar() {
@@ -11,15 +14,15 @@ function myNavbar() {
         <Navbar.Brand href="#home" className='fs-3 fw-medium ' >Green Guard</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav " />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto gap-4 ">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">News</Nav.Link>
-            <Nav.Link href="#link">Awareness</Nav.Link>
-            <Nav.Link href="#link">Guidelines</Nav.Link>
-            <Nav.Link href="#link">Complaints</Nav.Link>
+            <Nav className="ms-auto gap-4 ">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/news">News</Nav.Link>
+            <Nav.Link as={Link} to="/awareness">Awareness</Nav.Link>
+            <Nav.Link as={Link} to="/guidelines">Guidelines</Nav.Link>
+            <Nav.Link as={Link} to="/complaints">Complaints</Nav.Link>
+            <Button variant="success" className='ms-4'>Login</Button>{' '}
           </Nav>
         </Navbar.Collapse>
-        <Button variant="success" className='ms-4'>Login</Button>{' '}
       </Container>
     </Navbar>
   );
