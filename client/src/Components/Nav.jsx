a1a43c54e1c842f8a799546b6b42029364b0cae6
Naveen-gn <naveen2003gn@gmail.com>
2024-02-22 12:31:42 +0530

    Navbar modified

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,31 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+
 
 function myNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-dark navbar-dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className='fs-3 fw-medium ' >Green Guard</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto gap-4 ">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">News</Nav.Link>
+            <Nav.Link href="#link">Awareness</Nav.Link>
+            <Nav.Link href="#link">Guidelines</Nav.Link>
+            <Nav.Link href="#link">Complaints</Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <Button variant="success" className='ms-4'>Login</Button>{' '}
       </Container>
     </Navbar>
   );
