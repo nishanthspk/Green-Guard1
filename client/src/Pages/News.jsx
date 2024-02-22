6c25f88a47aa3cb6aa4606c84093e99edcd86a55
Naveen-gn <naveen2003gn@gmail.com>
2024-02-22 22:24:46 +0530

    News page fully completed

--- client/src/Pages/News.jsx
+++ client/src/Pages/News.jsx
@@ -9,91 +9,91 @@ export default function News() {
       <div class="d-flex flex-wrap gap-4 justify-content-center ">
 
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-107878656,width-300,resizemode-4/107878656.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">The top 6 Vastu-recommended plants for abundance</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '4',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>Vastu incorporates various plants like Money Plant, Jade Plant, Rubber Plant, Snake Plant, Peach Lily, and Golden Pothos for wealth, luck, positivity, stability, tranquillity, and financial growth, along with air purification and happiness.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-107158830,width-300,resizemode-4/107158830.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Delhi achieves 75% of total plantation target in Our contry.</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '4',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>The national capital has achieved 75% of its total plantation target this financial year, according to a report by Delhi Pollution Control Committee. A total of 79.7 lakh plantations were done by coordinating agencies.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-107805324,width-300,resizemode-4/107805324.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">How green is your Delhi? Wait a bit longer to put a number to it.</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '4',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>Municipal Corporation of Delhi conducted its first-ever tree census, identifying 1,56,067 lakh trees in 12 zones. The census is still ongoing, with 27% of the 250 wards covered so far. Narela zone has completed counting in all 16 wards.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-103097168,width-300,resizemode-4/103097168.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Govt plants over 1 crore trees across Telangana.</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '5',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>The state government has achieved an ambitious goal of planting over one crore trees across Telangana. The Million Tree Plantation campaign held as part of the celebrations of 75 years of Independence has planted 1,16,92,098 trees across the state.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-106546436,width-300,resizemode-4/106546436.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">New plant species discovered in Maharashtra's Pench Tiger Reserve: Forest official</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '3',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>During a survey at Gol Pahadi Island in the Pench Tiger Reserve of Maharashtra, a forest official announced the discovery of a new plant species within the Polygonum genus. For a year-long floral survey in Pench, 863 plant species from 554 genera and 117 families were documented, as reported by Dr Prabhu Nath Shuk.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-103526438,width-300,resizemode-4/103526438.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Cycle walk project: DDA to plant 4,000 trees soon.</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '5',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>Delhi Development Authority (DDA) plans to plant 4,000 trees in Phase I of the Delhi Cycle Walk project to be taken up from Malviya Nagar to Sangam Vihar. The project is divided into five phases and aims to provide cycling tracks along the greens of the city and elevated.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-106349814,width-300,resizemode-4/106349814.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Learn to communicate with the biosphere</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '5',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>This insightful piece delves into the vast spectrum of communication systems present in nature, extending beyond human-made technologies. It highlights the richness of exchanges among all life forms, including plants, animals, and microorganisms. The article emphasizes.</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-106299712,width-300,resizemode-4/106299712.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Lesser known secrets of Neem leaves: Health benefits, culinary uses, and some side effects too</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '3',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>Neem leaves offer a range of health benefits, including skincare benefits and promoting oral health. They can address issues like gum diseases and cavities and aid in wound healing. Neem leaves also have potential anti-cancer properties and promote hair health by addressing...</p>
             </div>
         </div>
       </div>
       
         <div class="">
-          <div class="card" style={{width:'18rem'}}>
-            <img src="" class="card-img-top" alt="..."/>
+          <div class="card" style={{width:'18rem', height:'23rem'}}>
+            <img src="https://static.toiimg.com/thumb/imgsize-123456,msid-106191272,width-300,resizemode-4/106191272.jpg" class="card-img-top" alt="..."/>
             <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <h5 class="card-title">Pilibhit: DM forms panel to probe felling of mango trees in ‘green belt’</h5>
+              <p class="card-text" style={{display: '-webkit-box',WebkitLineClamp: '4',WebkitBoxOrient: 'vertical',overflow: 'hidden'}}>A three-member panel has been formed by the Pilibhit district magistrate to investigate the illegal felling of 32 mango trees in a green belt. The panel, consisting of the divisional forest officer of Pilibhit Tiger Reserve and the city magistrate, will examine the status of the land.</p>
             </div>
         </div>
       </div>
@@ -119,6 +119,8 @@ export default function News() {
 
       </div>
       </div>
+
+      
 
   )
 }
