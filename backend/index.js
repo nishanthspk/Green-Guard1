74428f119ea81adfd9b40df5a164e959231953eb
Naveen-gn <naveen2003gn@gmail.com>
2024-02-26 08:46:53 +0530

    meta tags added and index.js message modified

--- backend/index.js
+++ backend/index.js
@@ -39,3 +39,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
+});
